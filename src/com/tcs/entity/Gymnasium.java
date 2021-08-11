e013481e33aeaa86837792881a82e57072d7d696
tcsrutvi <tcsrutvi@gmail.com>
2021-08-11 21:41:56 +0530

    first java code push

---
+++ src/com/tcs/entity/Gymnasium.java
@@ -0,0 +1,25 @@
+package com.tcs.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gymnasium {
+
+	static List<User> users = new ArrayList<User>();
+	public static void main(String args[]) {
+		
+		createUsers();
+	}
+	private static void createUsers() {
+		for(int i=0;i<10;i++) {
+			
+			User u1 = new User("Ram",42332432L, (byte)(10*i));
+			u1.setName("Ram"+ i);
+			u1.setAge((byte)(10 * i));
+			users.add(u1);
+	
+		}
+		System.out.println(users);
+		users.get(0).workout();
+	}
+}
---
+++ src/com/tcs/entity/User.java
@@ -0,0 +1,52 @@
+package com.tcs.entity;
+
+public class User {
+
+	private String name;
+	private Long mobileNumber;
+	private byte age;
+
+	// Keeping private to ensure data encapsulation
+	public void workout() {
+		System.out.println(name + "is working out");
+
+	}
+	
+
+	public User(String name, Long mobileNumber, byte age) {
+		super();
+		this.name = name;
+		this.mobileNumber = mobileNumber;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(Long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public byte getAge() {
+		return age;
+	}
+
+	public void setAge(byte age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + "]";
+	}
+
+}
